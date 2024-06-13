bbe5acd5b527dddb3673b3d9a6cdf088bfd2066b
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-06-12 21:09:00 -0500

    Ajustando espaciados y agregando el footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,15 @@
+import logoType from '../assets/images/logo/logo-jhorman-nieto.png';
+import '../styles/footer.css';
+
 export const Footer = () => {
   return (
-    <footer>Footer</footer>
+    <footer>
+      <div>
+        <img src={logoType} alt="Logo de Jhorman Nieto P" title='Logo de Jhorman Nieto' loading='lazy' width={200} height={200}/>
+      </div>
+      <ul>
+        <li><a href=""></a></li>
+      </ul>
+    </footer>
   )
 }
