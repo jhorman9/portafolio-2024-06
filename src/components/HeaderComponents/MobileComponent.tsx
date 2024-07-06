01630ca6034f4be52f5b38efe7bc885ee8c63386
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-07-06 18:37:01 -0500

    Ajustando el mobile de la landing page

--- src/components/HeaderComponents/MobileComponent.tsx
+++ src/components/HeaderComponents/MobileComponent.tsx
@@ -1,5 +1,4 @@
 import { ListNavigateComponent } from './ListNavigateComponent';
-import logoSVG from '../../assets/images/logo/logo-desktop.svg';
 import { SocialMediaComponent } from './SocialMediaComponent';
 
 interface MobileComponentProps {
@@ -10,9 +9,6 @@ export const MobileComponent: React.FC<MobileComponentProps> = ({ isOpen }) => {
 
   return (
       <div className='mobile-navbar' style={isOpen ? { display: 'block' } : { display: 'none' }}>
-        <div className='mobile-navbar_logo'>
-          <img src={logoSVG} alt="Logo de Jhorman Nieto" loading='eager' title='Logo de jhorman nieto' />
-        </div>
         <ListNavigateComponent />
         <div className='mobile-navbar_social-media d-none'>
           <SocialMediaComponent />
