0b8ce1e8a2bb60d4f9f4406a2015d53010a7ccd5
jhorman9 <79775604+jhorman9@users.noreply.github.com>
2024-07-10 21:17:04 -0500

    Ajustando el home

--- src/components/HomeComponents/SecondSectionComponent.tsx
+++ src/components/HomeComponents/SecondSectionComponent.tsx
@@ -41,7 +41,7 @@ export const SecondSectionComponent = () => {
         <div className='homePage_2-content__description description-txt'>
           <h2 className='title'>SOBRE M&#205;</h2>
           <p className='description-txt'>Radicado en Panamá, tengo {ageState.age} años y una pasión inquebrantable por la tecnología y el desarrollo web. Mi trayectoria profesional me ha permitido especializarme tanto en el frontend como en el backend, ofreciendo soluciones integrales y efectivas para mis clientes.</p>
-          <p className='description-txt'>Mi enfoque está en la creación de experiencias de usuario intuitivas y optimizadas, así como en el desarrollo de sistemas robustos y escalables. Siempre estoy en busca de nuevos conocimientos y desafíos, manteniéndome actualizado con las últimas tendencias y tecnologías para garantizar que mis proyectos sean innovadores y de alta calidad.</p>
+          <p className='description-txt without-margin'>Mi enfoque está en la creación de experiencias de usuario intuitivas y optimizadas, así como en el desarrollo de sistemas robustos y escalables. Siempre estoy en busca de nuevos conocimientos y desafíos, manteniéndome actualizado con las últimas tendencias y tecnologías para garantizar que mis proyectos sean innovadores y de alta calidad.</p>
           <button className='btn-primary' onClick={()=> navigate('/about')}>Leer más</button>
         </div>
       </div>
